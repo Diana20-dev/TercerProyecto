b93c9168270fcf0dc642942b8c0468ea29fa1485
Diana Karen Parra Sosa <dianaparra092@gmail.com>
2020-06-08 09:21:39 -0500

    Creando perfil de usuario

--- registration/views.py
+++ registration/views.py
@@ -1,5 +1,8 @@
 from .forms import UserCreationFormWithEmail
 from django.views.generic import CreateView
+from django.views.generic.base import TemplateView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django import forms
 
@@ -14,8 +17,16 @@ class SignUpView(CreateView):
     def get_form(self, form_class=None):
         form = super(SignUpView, self).get_form()
         #Modificar en tiempo real 
-        form.fields['username'].widget = forms.TextInput(attrs={'class':'form-control mb-2', 'placeholder':'Nombre de usuario'})
-        form.fields['email'].widget = forms.EmailInput(attrs={'class':'form-control mb-2', 'placeholder':'Dirección de correo electrónico'})
-        form.fields['password1'].widget = forms.PasswordInput(attrs={'class':'form-control mb-2', 'placeholder':'Contraseña'})
-        form.fields['password2'].widget = forms.PasswordInput(attrs={'class':'form-control mb-2', 'placeholder':'Repite la contraseña'})
+        form.fields['username'].widget = forms.TextInput(
+            attrs={'class':'form-control mb-2', 'placeholder':'Nombre de usuario'})
+        form.fields['email'].widget = forms.EmailInput(
+            attrs={'class':'form-control mb-2', 'placeholder':'Dirección de correo electrónico'})
+        form.fields['password1'].widget = forms.PasswordInput(
+            attrs={'class':'form-control mb-2', 'placeholder':'Contraseña'})
+        form.fields['password2'].widget = forms.PasswordInput(
+            attrs={'class':'form-control mb-2', 'placeholder':'Repite la contraseña'})
         return form
+
+@method_decorator(login_required, name='dispatch')
+class ProfileUpdate(TemplateView):
+    template_name = 'registration/profile_form.html'
--- webplayground/settings.py
+++ webplayground/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 #Auth redirects
-LOGIN_REDIRECT_URL = 'pages:pages'
+#LOGIN_REDIRECT_URL = 'pages:pages'
 LOGOUT_REDIRECT_URL = 'home'
 
 #Emails
@@ -138,3 +138,7 @@ if DEBUG:
 else:
     #Aquí hay que configurar un email real para producción 
     pass
+
+#Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
